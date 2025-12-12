81f68a7241089138ec5ca778bf494babd08cb45a
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2025-12-12 16:09:34 +0000

    [Finish]ルーティング作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'homes/home'
+
   devise_for :users
+
+  resources :
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
