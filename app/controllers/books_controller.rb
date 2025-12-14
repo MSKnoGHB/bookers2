ef8d8f4444fe468bc5b7a34418ba3baeb2b83ce3
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2025-12-14 14:48:31 +0000

    [Wip]form作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,25 @@
 class BooksController < ApplicationController
   
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create 
+    book = Book.new(book_params)
+    if book.save
+      redirect_to books_path
+    else
+      render :index
+    end
+  end
+
+
+
+  private
+
+  #ストパラ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,10 @@
 class Book < ApplicationRecord
 
   has_one_attached :post_image
-
   belongs_to :user
+
+  validates :title,presence: true
+  validates :body,presence: true
+  
 
 end
