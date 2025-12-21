91792841301806590997d95e9a016f26d5919541
MSKnoGHB <242181889+MSKnoGHB@users.noreply.github.com>
2025-12-21 09:48:45 +0000

    [Finish]User画像変更実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
-
-
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to user_path(user)
+    else
+      render :edit
+    end
   end
 
   def create
@@ -39,6 +46,12 @@ class UsersController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+
 
 
 end
